c4acf02479d7742c6bc00d9f74722b5e7b6b1ac4
Kazutoshi Shinkai <gokazy@gmail.com>
2016-04-20 10:07:55 +0900

    Slackへの通知を組込ました。

--- routes/locks.js
+++ routes/locks.js
@@ -1,14 +1,26 @@
 // lock.js
 var crypto = require('crypto');
+var debug = require('debug')('smalo:api');
 var devices = require('config').get('devices');
 var express = require('express');
+var slack = require('simple-slack-webhook');
+var util = require('util');
+
 var DoorStatus = require('../lib/door-status');
 var doorStatus = new DoorStatus();
+doorStatus.on('changeStatus', function(lockStatus){
+  // 鍵の状態を確認する
+  debug('[Door] change to ' + lockStatus);
+});
 
 var major = Math.floor(Math.random() * 65536);
 var minor = Math.floor(Math.random() * 65536);
-
 console.log({major: major, minor: minor});
+
+slack.init({
+  path: process.env['SLACK_WEBHOOK_URL'],
+  channel: '@shinkai'
+});
 
 var router = express.Router();
 router.param('hash', function(req, res, next, id){
@@ -44,12 +56,20 @@ router.param('hash', function(req, res, next, id){
 });
 
 router.get('/locks/unlocking/:hash', function(req, res, next){
+  // Slackへの通知
+  debug(req.device, 'slack:post', 'unlock');
+  slack.text(util.format('玄関のドアを解錠しました（ :key: %s）',  req.device.deviceName));
+
   console.log('requested: unlocking');
   res.send('200 OK');
 });
 
 
 router.get('/locks/locking/:hash', function(req, res, next){
+  // Slackへの通知
+  debug(req.device, 'slack:post', 'lock');
+  slack.text(util.format('玄関のドアを施錠しました（ :key: %s）',  req.device.deviceName));
+
   console.log('requested: locking');
   res.send('200 OK');
 });
