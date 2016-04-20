ea85984de0af12aeb06613ccc3dfa7ddf23134af
Kazutoshi Shinkai <gokazy@gmail.com>
2016-04-20 16:27:21 +0900

    APIのアクセスログをファイルに出力できるようにしました。

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var fileStreamRotator = require('file-stream-rotator');
 
 var locks = require('./routes/locks');
 
@@ -11,7 +13,21 @@ var server = app.listen(10080, function(){
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-app.use(logger('dev'));
+// ログ設定
+var logDirectory = __dirname + '/log';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// アクセスログ
+var accessLogStream = fileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+});
+app.use(logger('combined', {stream: accessLogStream}));
+if (app.get('env') === 'development') {
+  app.use(logger('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
