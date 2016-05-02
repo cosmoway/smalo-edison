afc9502fa03e0819e10ab65fd6872dac341f0b62
Kazutoshi Shinkai <gokazy@gmail.com>
2016-05-02 16:59:24 +0900

    iBeacon の発信を固定にしました。
    - major/minor を一定間隔で更新する機能を削除しました。

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+// app.js
+var config = require('config');
+var IBeacon = require('./lib/ibeacon');
 
-var locks = require('./routes/locks');
+// iBeacon スタート
+var ibeacon = new IBeacon(config.ibeacon);
 
-var app = express();
-
-var server = app.listen(10080, function(){
-  console.log("Node.js is listening to PORT:" + server.address().port);
-});
-
-app.use(logger('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/api', locks);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('404 Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-// production error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err.message);
-});
